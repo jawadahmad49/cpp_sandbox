79e6f53583d9c78045813fd174eb4379b2718a3c
Jawad Ahmad <72946302+jawad-2002@users.noreply.github.com>
2022-07-09 15:09:45 +0500

    Create variables.cpp

---
+++ cpp_basics/variables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //Variable is just a container for storing a value. It holds value, and then you can perform different operations it.
+  // Variables in C++ must have a defined data types. For example, if i have to initialize a variable of type integer. I'll just have to type as follows:
+  
+  
+  int a=25;
+  
+  
+  //There are multiple rules for defining a variable.
+  /* VARIABLE RULES:
+  - Variable names are case sensitive (name and NAME are different variables)
+  - Must start with a letter or an underscore
+  - Can have numbers but can not start with on */
+  
+ return 0; 
+}
