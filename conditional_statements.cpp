4af9bfb1bd8989a616b0835900431e45f64d65bc
Jawad Ahmad <72946302+jawadahmad49@users.noreply.github.com>
2023-08-16 10:47:33 +0500

    Update conditional_statements.cpp

--- conditional_statements.cpp
+++ conditional_statements.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  /*Conditional statements affect the natural flow of a program and by adding conditions it directs it in a certain way.
+  Let us understand it with practical examples.
+  */
+  int a =  4;
+  if (a==4)
+  {
+    //This block of code executes only if a == 4. Thus, conditional statements.
+    cout<<"a is equal to 4.";
+  }
+  // We can use logical operators to set conditions. Like,
+  if (a > 2)
+    //print something
+  
+  
 
+  return 0;
+
+}
