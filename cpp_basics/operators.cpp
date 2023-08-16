08efc84f547e2cf1cfe1b7cfa6b63b0b7d27a3af
Jawad Ahmad <72946302+jawadahmad49@users.noreply.github.com>
2023-08-16 10:42:25 +0500

    Update operators.cpp

--- cpp_basics/operators.cpp
+++ cpp_basics/operators.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 int main()
 {
-  //There are multiple ops in C++
+  //There are multiple operators in C++
+  //Arithmetic Operators are + , - , *, / and % which calculates the remainder.
+  // You can use them directly in expressions Like,
+  int a = 8;
+  int b = 4;
+  //Let's use all of them in a single line with an output statement.
+  // Try it right now in any code editor & see the output yourself;
+  cout<<a+b<<" " << a - b << " " <<a*b<<" "<<a/b<<" " <<a%b;
   
   
   
